d2b5780442e2f7fee4327af0abc490df654ec1d3
waterneedles <waterneedles@gmail.com>
2023-11-15 00:57:44 +0200

    Progress for the current Echo AI project

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+import { ClerkProvider } from '@clerk/nextjs'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Echo AI',
+  description: 'Echo AI is a platform for building and deploying AI models.',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </ClerkProvider>
+  )
+}
