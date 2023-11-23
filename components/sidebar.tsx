9a55734aab791572490d264ce3fc80f16ac7c21a
waterneedles <waterneedles@gmail.com>
2023-11-23 11:44:30 +0200

    Added mobile sidebar

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 import Link from "next/link";
 
 import { cn } from "@/lib/utils";
-import { LayoutDashboard } from "lucide-react";
+import { LayoutDashboard, MessageSquare, ImageIcon, VideoIcon, Music, Code, Settings } from "lucide-react";
 
 const monsterrat = Montserrat({ weight: "600", subsets: ["latin"]})
 
@@ -15,6 +15,41 @@ const routes = [
         icon: LayoutDashboard,
         href: "/dashboard",
         color: "text-sky-500",
+    },
+    {
+        label: "Conversation",
+        icon: MessageSquare,
+        href: "/conversation",
+        color: "text-violet-700",
+    },
+    {
+        label: "Image Generation",
+        icon: ImageIcon,
+        href: "/image",
+        color: "text-pink-700",
+    },
+    {
+        label: "Video Generation",
+        icon: VideoIcon,
+        href: "/video",
+        color: "text-orange-700",
+    },  
+    {
+        label: "Music Generation",
+        icon: Music,
+        href: "/music",
+        color: "text-emerald-500",
+    },   
+    {
+        label: "Code Generation",
+        icon: Code,
+        href: "/code",
+        color: "text-green-700",
+    }, 
+    {
+        label: "Settings",
+        icon: Settings,
+        href: "/settings",
     }
 ]
 
