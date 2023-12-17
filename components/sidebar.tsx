57108989a9eb0846e266340ad7ee1896fadb26a9
waterneedles <waterneedles@gmail.com>
2023-12-17 22:42:03 +0200

    Dashboard / AI integration

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -6,6 +6,7 @@ import Link from "next/link";
 
 import { cn } from "@/lib/utils";
 import { LayoutDashboard, MessageSquare, ImageIcon, VideoIcon, Music, Code, Settings } from "lucide-react";
+import { usePathname } from "next/navigation";
 
 const monsterrat = Montserrat({ weight: "600", subsets: ["latin"]})
 
@@ -54,6 +55,7 @@ const routes = [
 ]
 
 const Sidebar = () => {
+    const pathname = usePathname();
     return (
         <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
             <div className="px-3 py-2 flex">
@@ -70,10 +72,15 @@ const Sidebar = () => {
                     </h1>
                 </Link>
             </div>
-            <div className="space-y-1 px-3 py-2">
+            <div className="space-y-1">
                 {routes.map((route) => (
-                    <Link href={route.href} key={route.href} className="text-sm group flex p-3 w-full justify-start font-medium 
-                    ursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition">
+                    <Link 
+                    href={route.href} 
+                    key={route.href} 
+                    className={cn("text-sm group flex p-3 w-full justify-start font-medium", 
+                    "cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition",
+                    pathname === route.href ? "text-white bg-white/10" : "text-zinc-400"
+                    )}>
                         <div className="flex items-center flex-1">
                             <route.icon className={cn("h-5 w-5 mr-3", route.color)} />
                             {route.label}
